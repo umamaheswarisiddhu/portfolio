c2c93c4cb45ec897b286cd90112081ca86ffc2bb
umamaheswarisiddhu <umakutty231292@gmail.com>
2022-07-25 13:12:28 +0530

    commited

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,6 +3,8 @@ import projects from '../projectData';
 
 
 
+
+
 const Project = () => {
   return (
     <>
@@ -13,10 +15,19 @@ const Project = () => {
         <div className='my-7 space-y-24'>
           {projects.map((project, index) => (
             <article className='flex flex-wrap md:justify-between md:items-center' key={index}>
-             <picture className={index % 2 === 0 ? 'w-full md:w-6/12 shadow-lg ' : 'w-full md:w-6/12 shadow-lg md:order-1'}>
-             
-             </picture>
-              <div className='flex flex-col overflow-auto  space-y-3 my-3 mx-1 w-full md:w-5/12 '>
+              <picture className={index % 2 === 0 ? 'w-full md:w-6/12 shadow-lg ' : 'w-full md:w-6/12 shadow-lg md:order-1'}>
+                <source
+                  srcSet={`${project.image}.png 1920w,
+                        ${project.image}.png 768w,
+                        ${project.image}.png 320w`}
+                  sizes='(min-width: 768px) 50vw, 100vw'
+                  type='image/webp'
+                />
+                <source srcSet={`${project.image}-768.jpg 768w,`} sizes='(min-width: 768px) 50vw, 100vw' type='image/jpg' />
+
+                <img width='768px' height='575px' loading='lazy' alt={project.title} className='rounded-lg' />
+              </picture>
+             <div className='flex flex-col overflow-auto  space-y-3 my-3 mx-1 w-full md:w-5/12 '>
                 <h3 className='uppercase font-bold text-lg'>{project.title}</h3>
                 <p>{project.description}</p>
                 <div className='flex overflow-auto space-x-3 pb-2'>
@@ -35,7 +46,7 @@ const Project = () => {
                   </a>
                 </div>
               </div>
-            </article>
+              </article>
           ))}
         </div>
       </section>
--- src/projectData.js
+++ src/projectData.js
@@ -2,7 +2,7 @@
 const projects = [
     {
       title: 'CHAT APP',
-      image: '/images/chat.png',
+      image: './images/chat',
       description: ' A messaging app is a chat application or platform that enables users to instant message and connect with each other through their computers or mobile devices. ',      
       tools: ['React', 'bcrypt', 'MongoDB', 'socket.io', 'Express'],
       github: 'https://github.com/umamaheswarisiddhu/chatting-frd',
@@ -10,27 +10,27 @@ const projects = [
     },
     {
       title: 'PIZZA DELIVERY',
-      image: './images/tmdb-mockup',
+      image: './images/pizza',
       description: 'Discover the most popular, top rated and tasty pizza. Users can also find a  based on the some catagory.',
       tools: ['React','MongoDB','Express','JWT','razorpay'],
       github: 'https://github.com/umamaheswarisiddhu/yummy-pizza',
       link: 'https://yummypizzaaa.netlify.app/login',
     },
     {
-      title: 'CLOUD STORAGE',
-      image: './images/recipeApp-mockup',
-      description: 'Cloud storage is a cloud computing model that stores data on the Internet through a cloud computing provider who manages and operates data storage as a service',
-      tools: ['React','Redux','JWT','Express','mongodb'],
-      github: 'https://github.com/umamaheswarisiddhu/cloud-storage-client',
-      link: 'https://umacloudstorage.netlify.app/',
+      title: 'DOCKET APP',
+      image: './images/todo',
+      description: 'Microsoft To Do is a task management app to help you stay organized and manage your day-to-day. You can use Microsoft To Do to make shopping lists or task lists, take notes, record collections, plan an event, or set reminders to increase your productivity and focus on what matters to you.',
+      tools: ['React','JWT','Express','mongodb'],
+      github: 'https://github.com/umamaheswarisiddhu/docket-frd',
+      link: 'https://dockettodoapp.netlify.app/',
     },
     {
-      title: 'blogger',
-      image: './images/recipeApp-mockup',
+      title: 'BLOGGER',
+      image: './images/blogger',
       description: 'The Blogger mobile app allows you to post, edit, save, and view your blog posts.To post with the Blogger mobile app',
       tools: ['REACTjs','EXPRESSjs','NODEjs','Mongodb'],
-      github: 'https://github.com/umamaheswarisiddhu/blogger-client',
-      link: 'https://umabloggerapp.netlify.app/',
+      github: 'https://github.com/umamaheswarisiddhu/blogging-frd',
+      link: 'https://blogapp23.netlify.app/',
     },
   ];
   
